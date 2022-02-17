8165ba414d89b8642c35d3a92d1faa23edf368f8
Rahidul Islam <rahiseli@outlook.com>
2022-02-17 12:37:51 +0600

    update secret api key

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.conf import settings
 # Create your views here.
 
 class HomeView(View):
     template_name = 'core/index.html'
     def get(self, request):
+        
         return render(request,self.template_name)
